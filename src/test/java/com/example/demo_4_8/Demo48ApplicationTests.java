3b44fe098549febe9c7b1fefc2d0381bf30fd2b9
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-08-04 09:06:21 +0700

    thực hành ngày 4 tháng 8

---
+++ src/test/java/com/example/demo_4_8/Demo48ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_4_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo48ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
